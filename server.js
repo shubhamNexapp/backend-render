c5b9b944bfea5522c08d49c0986f9af01636accf
shubhamNexapp <shubham.s@nexapp.co.in>
2025-09-21 20:39:42 +0530

    add url mongo

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/merndb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://fixhomiteam_db_user:khjkn3QX7bHVHvZQ@cluster0.jygkq0h.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(5000, () => console.log('Server started on port 5000')))
   .catch(err => console.log(err));
